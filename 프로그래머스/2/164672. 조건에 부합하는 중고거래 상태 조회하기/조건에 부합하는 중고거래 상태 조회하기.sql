cd7da9a10cb1ad20dfd6ce45d494744af65142f2
kimheeseo <43293606+kimheeseo@users.noreply.github.com>
2024-03-28 00:21:22 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,13 @@
+ -- 코드를 입력하세요
+SELECT BOARD_ID, WRITER_ID, TITLE, PRICE, 
+        case when status = 'sale' then
+        '판매중'
+        when status = 'reserved' then
+        '예약중'
+        when status = 'done' then
+        '거래완료'
+        end as STATUS
+
+    from  USED_GOODS_BOARD 
+    where CREATED_DATE='2022-10-05'
+    order by BOARD_ID desc;
