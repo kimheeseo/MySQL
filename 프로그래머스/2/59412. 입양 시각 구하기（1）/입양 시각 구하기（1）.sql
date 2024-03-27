cb102143c215b003809ba99b8165851b2bab7d03
kimheeseo <43293606+kimheeseo@users.noreply.github.com>
2024-03-27 19:08:03 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,5 +1,2 @@
 -- 코드를 입력하세요
-SELECT hour(DATETIME) as HOUR, count(hour(DATETIME)) as COUNT from ANIMAL_OUTS 
-    where hour(DATETIME) >8 and hour(DATETIME)<20
-    group by hour(DATETIME)
-    order by hour(DATETIME);
+SELECT
