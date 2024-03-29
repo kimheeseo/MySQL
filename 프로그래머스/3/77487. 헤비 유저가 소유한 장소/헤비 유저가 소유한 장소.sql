e74c1663636368972fe00ea7c5ce4413b3b0de86
kimheeseo <43293606+kimheeseo@users.noreply.github.com>
2024-03-29 13:22:59 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -2,10 +2,7 @@ select ID, NAME, HOST_ID from PLACES
     where HOST_ID in (
     select HOST_ID from PLACES
     group by HOST_ID
-    having count(HOST_ID)>1
+    having count(ID)>1
+        -- 2개 등록해야 헤비유저니까, count(ID)>1로 설정
     )
     order by ID;
-
-
-
-    
