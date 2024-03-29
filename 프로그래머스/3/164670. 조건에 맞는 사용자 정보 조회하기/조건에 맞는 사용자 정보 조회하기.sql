0b58a49453cd00c20498d31df803d3bd446728af
kimheeseo <43293606+kimheeseo@users.noreply.github.com>
2024-03-29 13:34:32 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,8 +1,10 @@
--- 코드를 입력하세요
-select B.USER_ID as USER_ID, B.NICKNAME, concat(B.CITY,' ',B.STREET_ADDRESS1,' ',B.STREET_ADDRESS2) as '전체주소', concat(left(B.TLNO,3),'-',mid(B.TLNO,4,4),'-',right(B.TLNO,4)) as '전화번호' from USED_GOODS_USER B
-
-    where USER_ID in (SELECT A.WRITER_ID from USED_GOODS_BOARD A
+select B.USER_ID, B.NICKNAME, concat(B.CITY,' ',B.STREET_ADDRESS1, ' ',B.STREET_ADDRESS2) as 전체주소, concat(left(B.TLNO,3),'-',mid(B.TLNO,4,4),'-',right(B.TLNO,4))  from USED_GOODS_USER B
+    inner join USED_GOODS_BOARD A
+    on B.USER_ID = A.WRITER_ID
+    
+    where B.USER_ID in (select A.WRITER_ID from USED_GOODS_BOARD A    
     group by A.WRITER_ID
-    having count(A.WRITER_ID)>2
+    having count(A.WRITER_ID)>=3
     order by A.WRITER_ID)
-    order by USER_ID desc;
+    group by B.USER_ID, B.NICKNAME
+    order by B.USER_ID desc
