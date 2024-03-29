616ea46a6a92e6ba082d1c3c4702fd81888b93e4
kimheeseo <43293606+kimheeseo@users.noreply.github.com>
2024-03-29 13:36:37 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,7 +1,7 @@
 select A.NAME, A.DATETIME from ANIMAL_INS A
     left outer join ANIMAL_OUTS B
+-- left outer join을 쓴 이유는 입양을 못간 동물을 출력하기 위해서.
     on A.ANIMAL_ID = B.ANIMAL_ID
     where B.ANIMAL_ID is null
     order by A.DATETIME
     limit 3;
-    
